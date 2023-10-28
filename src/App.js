177a1a8ee3a2c27de798cb5a173bcc7c130f3eef
davideleo <99511112+davideleo5@users.noreply.github.com>
2023-10-28 13:07:22 +0200

    Added animation - text focus in
    Added animation and changed colors

--- src/App.js
+++ src/App.js
@@ -7,9 +7,7 @@ import './App.css';
 const App = () => {
   return (
     <div className='App'>
-      <div className='gradient__bg'>
-        <Header />
-      </div>
+      <Header />
       <What />
       <Shows />
       <CTA />
