260d18b5f6c9a3b04e0e8061ead322b299129eeb
davideleo <99511112+davideleo5@users.noreply.github.com>
2023-10-29 21:38:09 +0100

    Removed CTA component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import { Header, What, Shows, Footer } from './containers';
-import { Maintenance, CTA, Social } from './components';
+import { Maintenance, Social } from './components';
 import './App.css';
 
 const App = () => {
@@ -10,7 +10,6 @@ const App = () => {
       <Header />
       <What />
       <Shows />
-      <CTA />
       <Social />
       <Footer />
     </div>
--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,4 @@
 export { default as Contact } from './contact/Contact';
-export { default as CTA } from './cta/CTA';
 export { default as Maintenance } from './maintenance/Maintenance';
 export { default as Navbar } from './navbar/Navbar';
 export { default as Social } from './social/Social';
