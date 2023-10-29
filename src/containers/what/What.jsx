02c8ebed50ceb2c4eacca5b785c6bad94b4a9739
davideleo <99511112+davideleo5@users.noreply.github.com>
2023-10-29 17:50:57 +0100

    Fix animation chi siamo
    Set class text-flicker-in-glow when element is in view port

--- src/containers/what/What.jsx
+++ src/containers/what/What.jsx
@@ -1,15 +1,18 @@
-import React from 'react';
+import { React, useRef } from 'react';
+import { useInView } from 'react-intersection-observer';
 import './what.css';
 import background from "../../assets/chi-siamo.jpg";
 
 const What = () => {
+  const { ref, inView } = useInView();
+
   return (
     <div className='chi-siamo-container' style={{ backgroundImage: `url(${background})` }}>
-        <div className='chi-siamo-content section__padding'>
-          <h3 className='chi-siamo-title text-flicker-in-glow'>Chi siamo?</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ultrices venenatis tincidunt. Mauris quis metus ut justo fermentum rhoncus. Donec tempus diam ac odio tristique, vitae iaculis urna convallis. Suspendisse potenti. Duis vehicula tellus vel enim convallis bibendum. Donec leo augue, aliquet ac nisl vitae, faucibus pharetra diam. Cras sit amet neque nibh. Donec dictum orci est, a lobortis erat cursus ac. Pellentesque nec auctor nibh. Nullam in ex consequat, euismod tellus non, vulputate nisl. Sed fringilla pulvinar massa in rhoncus.</p>
-        </div>
+      <div className='chi-siamo-content section__padding'>
+        <h3 ref={ref} className={`chi-siamo-title ${inView ? 'text-flicker-in-glow' : ''}`}>Chi siamo?</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ultrices venenatis tincidunt. Mauris quis metus ut justo fermentum rhoncus. Donec tempus diam ac odio tristique, vitae iaculis urna convallis. Suspendisse potenti. Duis vehicula tellus vel enim convallis bibendum. Donec leo augue, aliquet ac nisl vitae, faucibus pharetra diam. Cras sit amet neque nibh. Donec dictum orci est, a lobortis erat cursus ac. Pellentesque nec auctor nibh. Nullam in ex consequat, euismod tellus non, vulputate nisl. Sed fringilla pulvinar massa in rhoncus.</p>
       </div>
+    </div>
   )
 }
 
