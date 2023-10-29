6ee26d9caacedaa71b045f848851f62312d42272
davideleo <99511112+davideleo5@users.noreply.github.com>
2023-10-29 21:36:34 +0100

    Shows section
    Shows section with coming soon text (waiting for shows dates)

--- src/components/index.js
+++ src/components/index.js
@@ -2,5 +2,4 @@ export { default as Contact } from './contact/Contact';
 export { default as CTA } from './cta/CTA';
 export { default as Maintenance } from './maintenance/Maintenance';
 export { default as Navbar } from './navbar/Navbar';
-export { default as Shows } from './shows/Shows';
 export { default as Social } from './social/Social';
--- src/containers/shows/Shows.jsx
+++ src/containers/shows/Shows.jsx
@@ -1,9 +1,17 @@
-import React from 'react';
+import { React, useRef } from 'react';
+import { useInView } from 'react-intersection-observer';
 import './shows.css';
+import background from "../../assets/shows.jpg";
 
 const Shows = () => {
+  const { ref, inView } = useInView({
+    triggerOnce: true
+  });
+
   return (
-    <div>Shows</div>
+    <div id='shows' className='shows-container' style={{ backgroundImage: `url(${background})` }}>
+      <h3 ref={ref} className={`shows-coming-soon-title ${inView ? 'slide-in-fwd-center' : ''}`}>COMING SOON...</h3>
+    </div>
   )
 }
 
