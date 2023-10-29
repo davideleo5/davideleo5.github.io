a7a7379fdf93f9573252c328f4ee60319bb11ad6
davideleo <99511112+davideleo5@users.noreply.github.com>
2023-10-29 18:30:09 +0100

    Applied animation to whole chi siamo section

--- src/containers/what/What.jsx
+++ src/containers/what/What.jsx
@@ -19,8 +19,8 @@ const What = () => {
 
   return (
     <div className='chi-siamo-container' style={{ backgroundImage: backgroundImageUrl }}>
-      <div className='chi-siamo-content section__padding'>
-        <h3 ref={ref} className={`chi-siamo-title ${inView ? 'text-flicker-in-glow' : ''} `}>Chi siamo?</h3>
+      <div className={`chi-siamo-content section__padding ${inView ? 'text-flicker-in-glow' : ''}`}>
+        <h3 ref={ref} className='chi-siamo-title'>Chi siamo?</h3>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ultrices venenatis tincidunt. Mauris quis metus ut justo fermentum rhoncus. Donec tempus diam ac odio tristique, vitae iaculis urna convallis. Suspendisse potenti. Duis vehicula tellus vel enim convallis bibendum. Donec leo augue, aliquet ac nisl vitae, faucibus pharetra diam. Cras sit amet neque nibh. Donec dictum orci est, a lobortis erat cursus ac. Pellentesque nec auctor nibh. Nullam in ex consequat, euismod tellus non, vulputate nisl. Sed fringilla pulvinar massa in rhoncus.</p>
       </div>
     </div>
