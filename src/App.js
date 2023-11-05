5e9ffa8c71e590f3823c25a12a327dd792358941
davideleo <99511112+davideleo5@users.noreply.github.com>
2023-11-05 22:50:53 +0100

    WIP - Footer
    added logo, email, instagram link, copyright

--- src/App.js
+++ src/App.js
@@ -10,7 +10,6 @@ const App = () => {
       <Header />
       <What />
       <Shows />
-      <Social />
       <Footer />
     </div>
   )
--- src/containers/footer/Footer.jsx
+++ src/containers/footer/Footer.jsx
@@ -1,9 +1,29 @@
 import React from 'react';
 import './footer.css';
+import logo from '../../assets/logo.png'
+import instagramLogo from '../../assets/instagram.svg'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className='footer'>
+      <div className='footer-content section__padding'>
+        <div className='column column-1'>
+          <a href='/'>
+            <img src={logo} alt='logo' />
+          </a>
+          <a className='mail' href="mailto:info@divino-musical.com">info@divino-musical.com</a>
+        </div>
+        <div className='column column-2'>
+        </div>
+        <div className='column column-3'>
+          <a href='https://instagram.com/divino_musical?igshid=YTQwZjQ0NmI0OA%3D%3D&utm_source=qr' target='blank'>
+            <img src={instagramLogo} alt='Instagram' />
+            <p>Instagram</p>
+          </a>
+        </div>
+      </div>
+      <p className='copyright'>Copyright © 2023 - Divino Musical</p>
+    </div>
   )
 }
 
