841182e2a22b0a6474468dedbd865144da15835c
davideleo <99511112+davideleo5@users.noreply.github.com>
2025-02-24 22:55:42 +0100

    Fix homepage show section

--- src/containers/shows/Shows.jsx
+++ src/containers/shows/Shows.jsx
@@ -9,8 +9,18 @@ const Shows = () => {
   });
 
   return (
-    <div id='shows' className='shows-container' style={{ backgroundImage: `url(${background})` }}>
-      <h2 ref={ref} className={`shows-coming-soon-title ${inView ? 'slide-in-fwd-center' : ''}`}>COMING SOON...</h2>
+    <div id='shows' className='home-shows-container' style={{ backgroundImage: `url(${background})` }}>
+      <div className='shows-content'>
+        <h2 ref={ref} className={`shows-coming-soon-title ${inView ? 'slide-in-fwd-center' : ''}`}>Prossimi spettacoli</h2>
+        <br />
+        <p>Teatro Carani - Sassuolo</p>
+        <p>3 e 4 maggio 2025</p>
+        <br />
+        <p>Non perderti la prima rappresentazione del nostro musical!</p>
+        <p>Ti aspettiamo per un’esperienza unica di musica, emozioni e spettacolo.</p>
+      </div>
+
+      <button><a href="/spettacoli">Scopri di più</a></button>
     </div>
   )
 }
