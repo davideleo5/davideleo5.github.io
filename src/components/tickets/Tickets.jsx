21f6dec8df1870d79d323c2d70c6639fee60cff3
davideleo <99511112+davideleo5@users.noreply.github.com>
2025-05-15 21:56:06 +0200

    Tickets
    Removed ticket info

--- src/components/tickets/Tickets.jsx
+++ src/components/tickets/Tickets.jsx
@@ -1,9 +1,18 @@
+import { Link } from "react-router-dom";
 import './tickets.css';
 
 const Tickets = () => {
   return (
     <div className='tickets-container container section__padding'>
       <h1>Biglietti</h1>
+
+      <div className='tickets-content'>
+        <p>Le prossime date verranno annunciate a breve.</p>
+        <p>Intanto ripercorri le emozioni delle prime due rappresentazioni</p>
+        <Link to='/gallery' className='button-link'>Sfoglia la gallery</Link>
+      </div>
+      
+      {/* <h1>Biglietti</h1>
 
       <div className='tickets-content'>
         <p>Vendita diretta presso la <a href='https://maps.app.goo.gl/NzYRFhQgwSkWdBxK9' target="_blank" rel="noopener noreferrer">canonica di Formigine</a>, ai seguenti orari:</p>
@@ -21,7 +30,7 @@ const Tickets = () => {
         <br />
         <br />
         <p>Biglietti disponibili anche online su <a href="https://www.eventbrite.com/cc/divino-il-musical-teatro-carani-3-4-maggio-2025-4180013" target="_blank" rel="noopener noreferrer">Eventbrite</a>.</p>
-      </div>
+      </div>*/}
     </div>
   )
 }
--- src/containers/shows/Shows.jsx
+++ src/containers/shows/Shows.jsx
@@ -13,12 +13,16 @@ const Shows = () => {
       <div ref={ref} className={`shows-content section__padding ${inView ? 'slide-in-fwd-center' : ''}`}>
         <h2 className='shows-coming-soon-title'>Prossimi spettacoli</h2>
         <br />
-        <p>Teatro Carani - Sassuolo</p>
+        <p>Le prossime date verranno annunciate a breve.</p>
+        <p>Intanto ripercorri le emozioni delle prime due rappresentazioni</p>
+        <Link to='/gallery' className='button-link'>Sfoglia la gallery</Link>
+
+        {/* <p>Teatro Carani - Sassuolo</p>
         <p>3 e 4 maggio 2025</p>
         <br />
         <p>Non perderti la prima rappresentazione del nostro musical!</p>
         <p>Ti aspettiamo per un’esperienza unica di musica, emozioni e spettacolo.</p>
-        <Link to='/spettacoli' className='button-link'>Scopri di più</Link>
+        <Link to='/spettacoli' className='button-link'>Scopri di più</Link>*/}
       </div>
     </div>
   )
